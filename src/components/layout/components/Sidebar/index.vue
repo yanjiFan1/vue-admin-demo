9d2844436fb2b2f7d25e2fcd24de7131b6fade0c
LAPTOP-SG4LQ13C\赵晓凡 <fanShaogithub@126.com>
2020-01-10 16:42:49 +0800

    sidebar更新

--- src/components/layout/components/Sidebar/index.vue
+++ src/components/layout/components/Sidebar/index.vue
@@ -1,62 +1,57 @@
+<!--
+ * @Description: 
+ * @Author: linZengFa
+ * @Date: 2019-11-14 14:15:58
+ * @LastEditTime: 2019-11-19 11:13:19
+ * @LastEditors: 
+ -->
 <template>
-  <el-scrollbar wrapClass="scrollbar-wrapper">
-    <el-menu
-      mode="vertical"
-      :show-timeout="200"
-      :default-active="$route.path"
-      :collapse="isCollapse"
-      background-color="#304156"
-      text-color="#bfcbd9"
-      active-text-color="#409EFF"
-    >
-      <sidebar-item v-for="route in permission_routers" :key="route.name" :item="route" :base-path="route.path"></sidebar-item>
-      
-    </el-menu>
-  </el-scrollbar>
+  <div>
+    <logo :collapse="isCollapse" />
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+        :default-active="activeMenu"
+        :collapse="isCollapse"
+        :background-color="variables.menuBg"
+        unique-opened
+        :text-color="variables.menuText"
+        :active-text-color="variables.menuActiveText"
+        >
+        <sidebar-item v-for="route in permission_routers" :key="route.path" :item="route" :base-path="route.path" />
+      </el-menu>  
+    </el-scrollbar>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import SidebarItem from './SidebarItem'
-
+import Logo from './Logo'
+import variables from '@/styles/variables.scss'
 export default {
-  components: { SidebarItem },
+  name: 'Index',
+  components: { SidebarItem, Logo },
   computed: {
-    ...mapGetters([
-      'permission_routers',
-      'sidebar'
-    ]),
+    ...mapGetters(['sidebar', 'permission_routers']),
+    activeMenu() {
+      const route = this.$route
+      const { meta, path } = route
+      // if set path, the sidebar will highlight the path you set
+      if (meta.activeMenu) {
+        return meta.activeMenu
+      }
+      return path
+    },
+    showLogo() {
+      return this.$store.state.settings.sidebarLogo
+    },
+    variables() {
+      return variables
+    },
     isCollapse() {
       return !this.sidebar.opened
     }
   },
-  data() {
-    return {
-      // permission_routers: [
-      //   { path: '/404', component: () => import('@/views/errorPage/404'), hidden: true },
-      //   { path: '/401', component: () => import('@/views/errorPage/401'), hidden: true },
-      //   {
-      //     path: '/',
-      //     redirect: 'dashboard',
-      //     children: [{
-      //       path: 'dashboard',
-      //       component: () => import('@/views/dashboard/index'),
-      //       name: 'dashboard',
-      //       meta: { title: 'dashboard', icon: 'dashboard', noCache: true }
-      //     }]
-      //   },
-      //   {
-      //     path: '/',
-      //     redirect: 'dashboard',
-      //     children: [{
-      //       path: 'dashboard',
-      //       component: () => import('@/views/dashboard/index'),
-      //       name: 'dashboard',
-      //       meta: { title: 'dashboard', icon: 'dashboard', noCache: true }
-      //     }]
-      //   }
-      // ]
-    }
-  }
+  methods: {}
 }
 </script>
